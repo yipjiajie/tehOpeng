77997b4a9fa6e61b7e876c89d87f010f248bf17d
dashingstag <dashing_stag@hotmail.com>
2015-11-14 00:32:44 +0800

    Update render.py

--- render.py
+++ render.py
@@ -9,14 +9,17 @@ test = cv2.imread("final0.jpg")
 frame_height= len(test)
 frame_width= len(test[0])
 
-print "frame height is ", frame_height
-print "frame width is ", frame_width
+print "frame height is ", 486
+print "frame width is ", 3668
 
-video =cv2.VideoWriter("000.avi", -1, 24, (frame_width/2,frame_height/2))
+video =cv2.VideoWriter("000.avi", -1, 24, (3668,486))
 
 for i in range (0,24):
 	img = cv2.imread("final"+str(i)+".jpg")
 	img=cv2.resize(img,(0,0),fx=0.5, fy=0.5)
+	frame_height= len(img)
+	frame_width= len(img[0])
+	print "pass ", i
 	video.write(img)
 video.release()
 
