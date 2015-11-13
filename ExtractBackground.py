5d1908fa2fa36dbf06b537fd2c4292fb7613b531
Chang Yan Qian <quan0203@gmail.com>
2015-11-13 22:44:43 +0800

    Added drawing leg spot of player with color

--- ExtractBackground.py
+++ ExtractBackground.py
@@ -9,18 +9,19 @@ import time
 import matplotlib.colors as colors
 
       
-def colorDistribution2(startx,starty,deltax,deltay,img):
+def colorDistributionBlue(startx,starty,deltax,deltay,img):
+  
   endx = int(startx)+deltax
   endy = int(starty)+deltay
   
   image = img[starty:endy,startx:endx]
   BLUE_MIN = np.array([100, 15, 50],np.uint8)
   BLUE_MAX = np.array([150, 255, 255],np.uint8)
+  
   hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
   frame_threshed = cv2.inRange(hsv_image, BLUE_MIN, BLUE_MAX)
-  cv2.imwrite('output2.jpg', frame_threshed)
-  hue, sat, val = hsv_image[:,:,0], hsv_image[:,:,1], hsv_image[:,:,2]
-  nonBlackHue = hue[(val>20)]
+  
+  # cv2.imwrite('output2.jpg', frame_threshed)
   
   numBluePixel = len(frame_threshed[frame_threshed>0])
   numPixel = deltax * deltay
@@ -31,48 +32,59 @@ def colorDistribution2(startx,starty,deltax,deltay,img):
 def colorDistributionGreen(startx,starty,deltax,deltay,img):
   endx = int(startx)+deltax
   endy = int(starty)+deltay 
+
   image = img[starty:endy,startx:endx]
   GREEN_MIN = np.array([35,125,170],np.uint8)
   GREEN_MAX = np.array([45,255,255],np.uint8)
+  
   hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
   frame_threshed = cv2.inRange(hsv_image, GREEN_MIN, GREEN_MAX)
-  cv2.imwrite('output3.jpg', frame_threshed)
+  
+  # cv2.imwrite('output3.jpg', frame_threshed)
+  
   numRedPixel = len(frame_threshed[frame_threshed>0])
   numPixel = deltax * deltay
   ratio = float(numRedPixel) / float(numPixel)
-  print "ratio:" , ratio
+  # print "ratio:" , ratio
   return ratio
 
 def colorDistributionLime(startx,starty,deltax,deltay,img):
   endx = int(startx)+deltax
   endy = int(starty)+deltay 
+  
   image = img[starty:endy,startx:endx]
   LIME_MIN = np.array([32,130,190],np.uint8)
   LIME_MAX = np.array([45,255,255],np.uint8)
+  
   hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
   frame_threshed = cv2.inRange(hsv_image, LIME_MIN, LIME_MAX)
-  cv2.imwrite('output3.jpg', frame_threshed)
+  
+  # cv2.imwrite('output3.jpg', frame_threshed)
+  
   numRedPixel = len(frame_threshed[frame_threshed>0])
   numPixel = deltax * deltay
   ratio = float(numRedPixel) / float(numPixel)
-  print "ratio:" , ratio
+  # print "ratio:" , ratio
   return ratio
 
 def colorDistributionRed(startx,starty,deltax,deltay,img):
   endx = int(startx)+deltax
   endy = int(starty)+deltay 
+  
   image = img[starty:endy,startx:endx]
   RED_MIN = np.array([1,100,50],np.uint8)
   RED_MAX = np.array([20,255,255],np.uint8)
+  
   hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
   frame_threshed = cv2.inRange(hsv_image, RED_MIN, RED_MAX)
-  cv2.imwrite('output3.jpg', frame_threshed)
+  
+  # cv2.imwrite('output3.jpg', frame_threshed)
+  
   numRedPixel = len(frame_threshed[frame_threshed>0])
   numPixel = deltax * deltay
   ratio = float(numRedPixel) / float(numPixel)
-  print "ratio:" , ratio
+  # print "ratio:" , ratio
   return ratio
-
 
 def colorDistribution(startx,starty,img):
   endx = int(startx)+3
@@ -85,13 +97,13 @@ def colorDistribution(startx,starty,img):
   print "max:" ,maxbin
   return maxbin
 
-##    *kernel = np.ones((3,3), np.uint8)
-##    color = []
-##    color.append(imgp[x][y])
-##    color.append(imgp[x+1][y+1])
-##    color.append(imgp[x-1][y-1])
-##    color.append(imgp[x+1][y-1])
-##    color.append(imgp[x-1][y+1])
+#    *kernel = np.ones((3,3), np.uint8)
+#    color = []
+#    color.append(imgp[x][y])
+#    color.append(imgp[x+1][y+1])
+#    color.append(imgp[x-1][y-1])
+#    color.append(imgp[x+1][y-1])
+#    color.append(imgp[x-1][y+1])
   
 # print "Enter the filename of the video (w/o extension)"
 # print "to have its background extracted:"
@@ -117,11 +129,11 @@ fgbg2 = cv2.BackgroundSubtractorMOG2()
 
 w = 10
 h = 10
-# w: 8475 h: 1078
-videoHeight = 1078
-videoWidth = 8475
-videoHeightOffsetTop = 235
-videoHeightOffsetBottom = 1030
+# w:7335 h: 972
+videoHeight = 972
+videoWidth = 7335
+videoHeightOffsetTop = 212
+videoHeightOffsetBottom = 940
 videoWidthOffsetLeft = 30
 videoWidthOffsetRight = videoWidth
 
@@ -132,6 +144,13 @@ frame_old = 0
 corners_old = 0
 corners_total = 0
 corners_count = 0
+
+cntBottomUnknown = []
+cntBottomRed = []
+cntBottomBlue = []
+cntBottomLime = []
+cntBottomGreen = []
+cntBottomWhite = []
 
 def filterCorners(corners):
   corners_filtered = np.array([c for c in corners
@@ -144,7 +163,6 @@ def filterCorners(corners):
   return corners_filtered
 
 def filterContours(contours):
-  # corners = np.array
   contours_filtered = np.array([c for c in contours
   if (c[0,1] >= videoHeightOffsetTop and c[0,1] <= videoHeightOffsetBottom
     and c[0,0] >= videoWidthOffsetLeft and c[0,0] <= videoWidthOffsetRight
@@ -153,6 +171,19 @@ def filterContours(contours):
     )])
 
   return contours_filtered
+
+def isOnField(x,y):
+  return (y >= 0 and y <= videoHeight
+          and x >= 0 and x <= videoWidth
+          # bottom
+          and y < (x * -0.0081) + 951.7662
+          # top
+          and y > (x * -0.00443) + 233.7784
+          # left
+          and y > (x * -0.2812) + 977.4330
+          # right
+          and y > (x * 0.223) - 864.7794
+          )
 
 def getForeground(frame, bw = 0):
 
@@ -178,79 +209,102 @@ def getExtremePoints(contour):
 
   return leftmost, rightmost, topmost, bottommost
 
+
+def trackPoints(previousFrame, nextFrame, old_points, canvas):
+
+  if(len(old_points) <= 0):
+    return canvas, old_points
+
+  # calculate optical flow
+  new_points, st, err = cv2.calcOpticalFlowPyrLK(previousFrame, nextFrame, 
+    prevPts=old_points, nextPts=None, maxLevel=1, winSize=(10,20), flags=cv2.OPTFLOW_LK_GET_MIN_EIGENVALS)
+  
+  # Select good points (st = status, 1 if corner is found in the next frame)
+  good_new = new_points[st==1]
+  good_old = old_points[st==1]
+
+  # drawing pink box to indicate new tracked points
+  # for i in range(0,len(good_new)):
+  #   x = int(good_new[i,0])
+  #   y = int(good_new[i,1])
+  #   cv2.circle(canvas, (x-w/2,y-h/2), 5, cv.RGB(203, 86, 194), 1)
+
+  good_new_points = good_new.reshape(-1,1,2)
+
+  return canvas, good_new_points
+
+
 print "Skipping First ",videoFrameOffset," frames"
 
 
 
 # for fr in range(0,frameCount-1):
-for fr in range(0,500):
+# for fr in range(30, 89):
 # for fr in range(0,0):
+for fr in range(430,1440):
 
   frame = cv2.imread("pics/final"+`fr`+".jpg",cv2.IMREAD_COLOR)
   
-  # _, frame = cap.read()
-
   original, foreColor, foreBW = getForeground(frame.copy())
+
+  print "Frame",fr
+
+  canvas = original
+
+  # Debug
+  # if(isFirstFrame < 1 and fr < 88):
+  #   continue
+
+  # cv2.line(canvas, (94, 951), (7332, 892), cv.RGB(255,0,0), 1)
+  # cv2.line(canvas, (2722, 212), (4809, 203), cv.RGB(255,0,0), 1)
+  # cv2.line(canvas, (94,951), (2722, 212), cv.RGB(255,0,0), 1)
+  # cv2.line(canvas, (4809, 203), (7332, 770), cv.RGB(255,0,0), 1)
+
+
+  # ret,thresh = cv2.threshold(foreBW,127,255,0)
   
-  # cv2.imshow('frame', result)
+  # kernel = np.ones((1,1), 'uint8')
+  # dilated = cv2.dilate(np.array(thresh, dtype='uint8'), kernel)
+  # contours,hierarchy = cv2.findContours(dilated, cv.CV_RETR_TREE, cv.CV_CHAIN_APPROX_SIMPLE)
+  
+  # cntBottomUnknown = []
+  # cntBottomRed = []
+  # cntBottomBlue = []
+  # cntBottomLime = []
+  # cntBottomGreen = []
+  # cntBottomWhite = []
+
+  # for cnt in contours:
+  #   area = cv2.contourArea(cnt)     
+  #   bottomX, bottomY = tuple(cnt[cnt[:,:,1].argmax()][0])
+
+  #   # print x,"-",y,":",area
+
+  #   # skip small contour area
+  #   if((bottomY > 300 and area < 100) or (bottomY > 400 and area < 180) or (area < 100)):
+  #    continue
+    
+  #   # skip contour that is not on the field
+  #   if(isOnField(bottomX, bottomY) <= 0):
+  #     continue
+
+  #   cv2.rectangle(canvas, (bottomX-5, bottomY-5), (bottomX+5,bottomY+5), cv.RGB(255,0,0), 1)
+
+
+  # cv2.imwrite("out.jpg",original)
+  # # cv2.imshow('frame', canvas)
   # cv2.waitKey(1)
   # continue
 
-  if(isFirstFrame < 1 and fr < videoFrameOffset):
-    cv2.imshow('frame', original)
-    cv2.waitKey(1)
+
+  # Actual Code
+  if(isFirstFrame < 1 and fr < 480):
+    # cv2.imshow('frame', original)
+    # cv2.waitKey(1)
     continue
 
-  # print "start find contours"
-  # # frame_old = cv2.cvtColor(foreColor, cv2.COLOR_BGR2GRAY)
-  # # corners = cv2.goodFeaturesToTrack(frame_old, minDistance=35,
-  # #   maxCorners = 1000, qualityLevel=0.08, blockSize=9, useHarrisDetector=0, k=0.04)
 
-  # ret,thresh = cv2.threshold(foreBW,127,255,0)
-  # contours,hierarchy = cv2.findContours(thresh, cv.CV_RETR_TREE, cv.CV_CHAIN_APPROX_TC89_L1)
-  
-  # # cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
-  # # cv2.imshow('frame', frame_old)
-
-  # cntBottom = []
-  # for cnt in contours:
-  #   area = cv2.contourArea(cnt)
-    
-  #   x, y = tuple(cnt[cnt[:,:,1].argmax()][0])
-  #   # print x,"-",y,":",area
-
-  #   if((y > 300 and area < 100) or (y > 400 and area < 180) or (area < 100)):
-  #    continue
-  #   # cv2.rectangle(thresh, (x,y), (x+5,y+5), cv.RGB(255,0,0), 1)
-  #   cntBottom.append([x,y])
-
-  # contours_filtered = filterContours(cntBottom)
-
-  # for cpt in contours_filtered:
-  #   x = cpt[0]
-  #   y = cpt[1]
-  #   cv2.rectangle(original, (x,y), (x+5,y+5), cv.RGB(255,0,0), 1)
-
-  # corners_old = filterCorners(cntBottom)
-  # # corners_old = corners
-
-  # for i in range(0,len(corners_old)):
-  #   x = int(corners_old[i,0,0])
-  #   y = int(corners_old[i,0,1])
-  #   cv2.rectangle(original, (x-w/2,y-h/2), (x+w/2,y+h/2), cv.RGB(255, 0, 0), 1)
-
-  # cv2.line(frame, (videoWidthOffsetLeft, videoHeightOffsetTop), (videoWidthOffsetRight, videoHeightOffsetTop), cv.RGB(255,0,0), 1)
-  # cv2.line(frame, (videoWidthOffsetLeft,videoHeightOffsetBottom), (videoWidthOffsetRight, videoHeightOffsetBottom), cv.RGB(255,0,0), 1)
-  # cv2.line(frame, (0,videoHeightOffsetBottom), (3750, 0), cv.RGB(255,0,0), 1)
-  # cv2.line(frame, (4300, 0), (videoWidthOffsetRight, videoHeightOffsetBottom-100), cv.RGB(255,0,0), 1)
-
-  # cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
-  # cv2.imwrite("thresh.jpg", thresh)
-  # cv2.imwrite("original.jpg", original)
-  # cv2.imshow('frame', original)
-  # continue
-
-  if(isFirstFrame < 1 or corners_count < 21 or  corners_count > 23 or fr % 12 == 0):
+  if(isFirstFrame < 1 or fr % 4 == 0):
 
     startTime = time.time()
 
@@ -259,48 +313,85 @@ for fr in range(0,500):
     kernel = np.ones((1,1), 'uint8')
     dilated = cv2.dilate(np.array(thresh, dtype='uint8'), kernel)
     contours,hierarchy = cv2.findContours(dilated, cv.CV_RETR_TREE, cv.CV_CHAIN_APPROX_SIMPLE)
-    #contours,hierarchy = cv2.findContours(thresh, cv.CV_RETR_TREE, cv.CV_CHAIN_APPROX_SIMPLE)
     
-    cntBottom = []
+    cntBottomUnknown = []
+    cntBottomRed = []
+    cntBottomBlue = []
+    cntBottomLime = []
+    cntBottomGreen = []
+    cntBottomWhite = []
+
     for cnt in contours:
       area = cv2.contourArea(cnt)     
-      x, y = tuple(cnt[cnt[:,:,1].argmax()][0])
+      bottomX, bottomY = tuple(cnt[cnt[:,:,1].argmax()][0])
+
       # print x,"-",y,":",area
 
-      if((y > 300 and area < 100) or (y > 400 and area < 180) or (area < 100)):
+      # skip small contour area
+      if((bottomY > 300 and area < 100) or (bottomY > 400 and area < 180) or (bottomY > 700 and area < 250) or (area < 100)):
        continue
-      # cv2.rectangle(thresh, (x,y), (x+5,y+5), cv.RGB(255,0,0), 1)
-      cntBottom.append([[x,y]])
-##      rect = cv2.minAreaRect(cnt)
-##      box = cv2.cv.BoxPoints(rect)
-##      box = np.int0(box)
-##      cv2.drawContours(original,[box],0,(0,100,255),2)
-      x,y,w2,h2 = cv2.boundingRect(cnt)
-      cv2.rectangle(original,(x,y),(x+w2,y+h2),(255,0,255),2)
+      
+      # skip contour that is not on the field
+      if(isOnField(bottomX, bottomY) <= 0):
+        continue
+
+      # cntBottom.append([[x,y]])
+      # rect = cv2.minAreaRect(cnt)
+      # box = cv2.cv.BoxPoints(rect)
+      # box = np.int0(box)
+      # cv2.drawContours(original,[box],0,(0,100,255),2)
+
+      boundX, boundY, boundW, boundH = cv2.boundingRect(cnt)
+
+      # draw bounding box
+      # cv2.rectangle(canvas,(boundX, boundY),(boundX+boundW, boundY+boundH),(255,0,255),2)
+      
       moments = cv2.moments(cnt)  
+
       if moments['m00']!=0:
         cx = int(moments['m10']/moments['m00'])
         cy = int(moments['m01']/moments['m00'])      
-        colorval = colorDistribution2(x,y,w2,h2,foreColor)
-        colorval2 = colorDistributionRed(x,y,w2,h2,foreColor)
-        colorvalLime = colorDistributionLime(x,y,w2,h2,foreColor)
-        colorvalGreen = colorDistributionGreen(x,y,w2,h2,foreColor)
-        #colorval = colorDistribution(cx,cy,foreColor)
-##        print "x: " ,cx," y: ",cy," hue: ", colorval
-##        cv2.rectangle(original, (cx,cy), (cx+10,cy+10), cv.RGB(25,15,50), 1)
+
+        colorvalBlue = colorDistributionBlue(boundX, boundY, boundW, boundH, foreColor)
+        colorvalRed = colorDistributionRed(boundX, boundY, boundW, boundH, foreColor)
+        colorvalLime = colorDistributionLime(boundX, boundY, boundW, boundH, foreColor)
+        colorvalGreen = colorDistributionGreen(boundX, boundY, boundW, boundH, foreColor)
         
-        if(colorval >0.01):
-          cv2.rectangle(original, (cx,cy), (cx+10,cy+10), cv.RGB(255,0,0), 1)
-        elif(colorval2>0.05):
-          cv2.rectangle(original, (cx,cy), (cx+10,cy+10), cv.RGB(0,0,255), 1)
-        elif(colorvalLime>0.05):
-          cv2.rectangle(original, (cx,cy), (cx+10,cy+10), cv.RGB(255,255,255), 1)
-        elif(colorvalGreen>0.05):
-          cv2.rectangle(original, (cx,cy), (cx+10,cy+10), cv.RGB(0,255,0), 1)
+        if(colorvalBlue > 0.01):
           
+          # cv2.rectangle(canvas, (cx,cy), (cx+10,cy+10), cv.RGB(255,0,0), 1)
+          cntBottomBlue.append([[bottomX, bottomY]])
+        
+        elif(colorvalRed > 0.05):
+        
+          # cv2.rectangle(canvas, (cx,cy), (cx+10,cy+10), cv.RGB(0,0,255), 1)
+          cntBottomRed.append([[bottomX, bottomY]])
+
+        elif(colorvalLime>0.05):
+        
+          # cv2.rectangle(canvas, (cx,cy), (cx+10,cy+10), cv.RGB(255,255,255), 1)
+          cntBottomLime.append([[bottomX, bottomY]])
+
+        elif(colorvalGreen>0.05):
+        
+          # cv2.rectangle(canvas, (cx,cy), (cx+10,cy+10), cv.RGB(0,255,0), 1)
+          cntBottomGreen.append([[bottomX, bottomY]])
+
+        else:
+
+          # cv2.rectangle(canvas, (cx,cy), (cx+10,cy+10), cv.RGB(0,0,0), 1)
+          cntBottomUnknown.append([[bottomX, bottomY]])
+
     # print "corners:\n", np.array(cntBottom, dtype='f')
     
-    contours_filtered = filterContours(np.array(cntBottom, dtype='f'))
+    # contours_filtered = filterContours(np.array(cntBottom, dtype='f'))
+    
+    cntBottomUnknown = np.array(cntBottomUnknown, dtype='f')
+    cntBottomRed = np.array(cntBottomRed, dtype='f')
+    cntBottomBlue = np.array(cntBottomBlue, dtype='f')
+    cntBottomLime = np.array(cntBottomLime, dtype='f')
+    cntBottomGreen = np.array(cntBottomGreen, dtype='f')
+    cntBottomWhite = np.array(cntBottomWhite, dtype='f')
 
     # find corners in the first frame
     frame_old = cv2.cvtColor(foreColor, cv2.COLOR_BGR2GRAY)
@@ -308,52 +399,89 @@ for fr in range(0,500):
       # maxCorners = 1000, qualityLevel=0.08, blockSize=9, useHarrisDetector=0, k=0.04)
     # corners_old = filterCorners(corners)
 
-    corners_old = contours_filtered
-    corners_count = len(corners_old)
-    corners_total = corners_count
+    # corners_old = contours_filtered
+    # corners_old = np.concatenate((cntBottomUnknown, cntBottomRed, cntBottomBlue, cntBottomLime, cntBottomGreen, cntBottomWhite))
+    # corners_count = len(cntBottomUnknown) + len(cntBottomUnknown) + len(cntBottomRed) + len(cntBottomBlue) + len(cntBottomLime) + len(cntBottomGreen) + len(cntBottomWhite)
+    # corners_count = len(corners_old)
+    # corners_total = corners_count
 
     print "finished find contours: ", time.time() - startTime, "(sec)"
 
     isFirstFrame = 1;
   
-  cv2.imwrite("out.jpg",original)
-  if(isFirstFrame > 0 and isFirstFrame <= 0):
+
+  if(isFirstFrame > 0):
 
     frame_gray = cv2.cvtColor(foreColor, cv2.COLOR_BGR2GRAY)
     
     # print "corners:\n", corners_old
 
-    # calculate optical flow
-    corners, st, err = cv2.calcOpticalFlowPyrLK(frame_old, frame_gray, 
-      prevPts=corners_old, nextPts=None, maxLevel=1, winSize=(10,20), flags=cv2.OPTFLOW_LK_GET_MIN_EIGENVALS)
+    canvas, newPoints = trackPoints(frame_old, frame_gray, cntBottomRed, canvas)
+    cntBottomRed = newPoints.copy()
     
-    # Select good points (st = status, 1 if corner is found in the next frame)
-    good_new = corners[st==1]
-    good_old = corners_old[st==1]
+    if(len(cntBottomRed) > 0):
+      for pt in cntBottomRed[:,0,:]:
+        x = int(pt[0])
+        y = int(pt[1])
+        cv2.rectangle(canvas, (x-5, y-5), (x+5,y+5), cv.RGB(255,0,0), 1)
 
+    canvas, newPoints = trackPoints(frame_old, frame_gray, cntBottomBlue, canvas)
+    cntBottomBlue = newPoints.copy()
+  
+    if(len(cntBottomBlue) > 0):
+      for pt in cntBottomBlue[:,0,:]:
+        x = int(pt[0])
+        y = int(pt[1])
+        cv2.rectangle(canvas, (x-5, y-5), (x+5,y+5), cv.RGB(0,0,255), 1)
 
-    # drawing blue box to indicate new tracked points
-    for i in range(0,len(good_new)):
-      x = int(good_new[i,0])
-      y = int(good_new[i,1])
-      cv2.rectangle(original, (x-w/2,y-h/2), (x+w/2,y+h/2), cv.RGB(0, 0, 255), 1)
+    canvas, newPoints = trackPoints(frame_old, frame_gray, cntBottomGreen, canvas)
+    cntBottomGreen = newPoints.copy()
 
-    # drawing red box to indicate old tracked points
-    for i in range(0,len(good_old)):
-      x = int(good_old[i,0])
-      y = int(good_old[i,1])
-      cv2.rectangle(original, (x-w/2,y-h/2), (x+w/2,y+h/2), cv.RGB(255, 0, 0), 1)
+    if(len(cntBottomGreen) > 0):
+      for pt in cntBottomGreen[:,0,:]:
+        x = int(pt[0])
+        y = int(pt[1])
+        cv2.rectangle(canvas, (x-5, y-5), (x+5,y+5), cv.RGB(0,255,0), 1)
 
-    cv2.imwrite("out.jpg",original)
-    #cv2.imshow('frame', original)
-    cv2.waitKey(1)
+    canvas, newPoints = trackPoints(frame_old, frame_gray, cntBottomLime, canvas)
+    cntBottomLime = newPoints.copy()
+
+    if(len(cntBottomLime) > 0):
+      for pt in cntBottomLime[:,0,:]:
+        x = int(pt[0])
+        y = int(pt[1])
+        cv2.rectangle(canvas, (x-5, y-5), (x+5,y+5), cv.RGB(255,255,255), 1)
+
+    canvas, newPoints = trackPoints(frame_old, frame_gray, cntBottomWhite, canvas)
+    cntBottomWhite = newPoints.copy()
+
+    canvas, newPoints = trackPoints(frame_old, frame_gray, cntBottomUnknown, canvas)
+    cntBottomUnknown = newPoints.copy()
+
+    if(len(cntBottomUnknown) > 0):
+      for pt in cntBottomUnknown[:,0,:]:
+        x = int(pt[0])
+        y = int(pt[1])
+        cv2.rectangle(canvas, (x-5, y-5), (x+5,y+5), cv.RGB(0,0,0), 1)
 
 
     # Now update the previous frame and previous points
     frame_old = frame_gray.copy()
-    corners_old = good_new.reshape(-1,1,2)
-    corners_count = len(corners_old)
+    # corners_old = good_new.reshape(-1,1,2)
+    # corners_old = np.concatenate((cntBottomUnknown, cntBottomRed, cntBottomBlue, cntBottomLime, cntBottomGreen, cntBottomWhite))
+    # corners_count = len(corners_old)
 
+    # corners_count = len(cntBottomUnknown) + len(cntBottomUnknown) + len(cntBottomRed) + len(cntBottomBlue) + len(cntBottomLime) + len(cntBottomGreen) + len(cntBottomWhite)
+
+
+  cv2.line(canvas, (94, 951), (7332, 892), cv.RGB(255,0,0), 1)
+  cv2.line(canvas, (2722, 212), (4809, 203), cv.RGB(255,0,0), 1)
+  cv2.line(canvas, (94,951), (2722, 212), cv.RGB(255,0,0), 1)
+  cv2.line(canvas, (4809, 203), (7332, 770), cv.RGB(255,0,0), 1)
+
+  cv2.imwrite("out.jpg", canvas)
+  # cv2.imshow('frame', canvas)
+  cv2.waitKey(1)
 
 print "End"
 cv2.waitKey(0)
